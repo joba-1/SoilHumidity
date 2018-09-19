8a89d023f9255040df282dcb25019afd1998d751
joba1 <Joachim.Banzhaf@gmail.com>
2018-09-19 16:23:58 +0200

    * Led class * parametrized sensor calibration data * Boot message includes program and version

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,59 @@
 #include <Arduino.h>
 
-// Just to make sure it is off
 #include <ESP8266WiFi.h>
 
-#define LED_PIN D4
+#define PROGRAM "Moisture"
+#define VERSION "1.0"
 
-// NodeMCU led D4 is inverted
-#define LED_ON  LOW
-#define LED_OFF HIGH
+#define A0_WET 875
+#define A0_DRY 445
 
-bool ledState = LED_OFF;
+#define PERCENT_DRY_LED_ON  25
+#define PERCENT_WET_LED_OFF 30
+
+template < bool Inverted > class TLed {
+public:
+  typedef enum { OFF = false, ON = true } t_state;
+
+  TLed( int pin ) : _pin(pin) {};
+
+  void on( void ) const { digitalWrite(_pin, Inverted ? LOW : HIGH ); };
+  void off( void ) const { digitalWrite(_pin, Inverted ? HIGH : LOW ); };
+  void set( t_state state ) const { if( state ) on(); else off(); };
+  void begin( t_state state = OFF ) const { pinMode(_pin, OUTPUT); set(state); };
+
+private:
+  int _pin;
+};
+
+typedef class TLed < true > Led;
+
+Led led(D4);
+Led::t_state ledState = Led::OFF;
 
 void setup(void) {
   Serial.begin(115200);
   WiFi.mode(WIFI_OFF);
-  pinMode(LED_PIN, OUTPUT);
-  Serial.println("\nBooted.");
-  digitalWrite(LED_PIN, ledState); // Serial messes with led state
+  Serial.printf("\nBooted %s %s\n", PROGRAM, VERSION);
+  led.begin(ledState);
 }
 
 void loop( void ) {
   // Capacitive sensor pin on A0 yields 875 in water and 445 in air
   // Translate this to a range from 100% (wet) to 0% (dry)
-  int humidity = map(constrain( analogRead(A0), 445, 875 ), 875, 445, 0, 100);
+  int humidity = map(constrain( analogRead(A0), A0_DRY, A0_WET ), A0_WET, A0_DRY, 0, 100);
 
-  Serial.print("humidity: ");
+  Serial.print("humidity %: ");
   Serial.println(humidity);
 
-  if( humidity < 20 ) {
-    ledState = LED_ON;   // Time to give plants some water
+  if( humidity < PERCENT_DRY_LED_ON ) {
+    ledState = Led::ON;   // Time to give plants some water
   }
-  else if( humidity > 25 ) {
-    ledState = LED_OFF;  // Soil is moist
+  else if( humidity > PERCENT_WET_LED_OFF ) {
+    ledState = Led::OFF;  // Soil is moist
   }
-  digitalWrite(LED_PIN, ledState);
+
+  led.set(ledState);
 
   delay(1*1000);
 }
